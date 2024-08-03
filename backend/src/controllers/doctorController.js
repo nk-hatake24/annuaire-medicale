7bfecf5c887a55058c2a3acf70a1a61bc068d1d2
Nkenla Michel (mitcode) <nkenlamichel@gmail.com>
2024-08-03 12:06:54 +0200

    createDoctor contoller

---
+++ backend/src/controllers/doctorController.js
@@ -0,0 +1,22 @@
+const Doctor = require('../models/doctorModel');
+
+
+const createDoctor = async (req, res) => {
+    try {
+      const doctor = new Doctor(req.body);
+      await doctor.save();
+      res.status(201).json(doctor);
+    } catch (error) {
+      if (error.name === 'ValidationError') {
+        res.status(400).json({ message: error.message });
+      } else if (error.code === 11000) {
+        res.status(409).json({ message: 'License number already exists' });
+      } else {
+        res.status(500).json({ message: 'Internal server error' });
+      }
+    }
+  }
+
+
+
+module.exports={createDoctor}
