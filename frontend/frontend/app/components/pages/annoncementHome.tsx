641810b69401dc400e00aa0b39b16c9b26a29e74
nk-hatake24 <nkenlamichel@gmail.com>
2024-11-01 12:25:01 +0100

    center btn annoncement

--- frontend/frontend/app/components/pages/annoncementHome.tsx
+++ frontend/frontend/app/components/pages/annoncementHome.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function AnnouncementHome() {
   return (
     <section className="announcementHomePage  lg:h-screen flex-row flex  bg-slate-200">
-      <div className="lg:w-1/2 w-full h-screen flex flex-col gap-8 justify-center px-4 lg:pl-28 md:pr-8">
+      <div className="lg:w-1/2 w-full h-screen flex flex-col gap-8 justify-center items-center px-4 lg:pl-28 md:pr-8">
         <h2 className="text-3xl tracking-tight font-bold self-center">
           Share Announcement
         </h2>
@@ -14,7 +14,7 @@ export default function AnnouncementHome() {
           alerts, directly with you.
         </p>
         <Link href={"/announcement"}>
-          <button className="p-2 text-gray-50 bg-blue-600 hover:bg-transparent border self-center border-blue-600 hover:text-blue-600  font-semibold max-w-80 rounded-sm">
+          <button className="p-2 text-gray-50 bg-blue-600 hover:bg-transparent border  border-blue-600 hover:text-blue-600  font-semibold max-w-80 rounded-sm">
             view latest announcements.
           </button>
         </Link>
