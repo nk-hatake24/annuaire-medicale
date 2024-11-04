31216fb6c39e4ab29318707a9d25e7a518b9715e
nk-hatake24 <nkenlamichel@gmail.com>
2024-11-04 21:01:08 +0100

    spinnner center

--- frontend/frontend/components/spinnner.tsx
+++ frontend/frontend/components/spinnner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner(){
     return(
         
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center h-full bg-gray-100">
         <div className="animate-spin rounded-full h-24 w-24 border-t-4 border-blue-500"></div>
         </div>
     
